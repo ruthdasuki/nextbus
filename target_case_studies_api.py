7168cb0736cad4428614286b2c39fd9434543788
ruthdasuki@gmail.com <ruthdasuki@gmail.com>
2020-01-20 21:46:36 -0600

    Add relevant apis

--- target_case_studies_api.py
+++ target_case_studies_api.py
@@ -1,6 +1,14 @@
 import requests
-import json
 
-url = "https://svc.metrotransit.org/NexTrip/Routes?format=json"
+getRoutes = "https://svc.metrotransit.org/NexTrip/Routes?format=json"
+getDirections = "https://svc.metrotransit.org/NexTrip/Directions/5?format=json"
+getStops = "https://svc.metrotransit.org/NexTrip/Stops/5/4?format=json"
+getDepartures = "https://svc.metrotransit.org/NexTrip/11167?format=json"
+getTimepointDepartures = "https://svc.metrotransit.org/NexTrip/5/4/7SOL?format=json"
 
-print(requests.get(url).json())
+print(requests.get(getRoutes).json())
+print(requests.get(getDirections).json())
+print(requests.get(getStops).json())
+print(requests.get(getDepartures).json())
+print(requests.get(getTimepointDepartures).json())
+
