142b3310dfe9d91b908dc37d39b4c088600ff732
ruthdasuki@gmail.com <ruthdasuki@gmail.com>
2020-01-21 10:54:19 -0600

    Mock 2nd example

--- target_case_studies_api.py
+++ target_case_studies_api.py
@@ -1,14 +1,27 @@
 import requests
+from requests.exceptions import HTTPError
+
+bus_route = input("Enter your BUS ROUTE: ")
+#bus_stop_name = input("Enter your BUS STOP NAME: ")
+#direction = input("Enter your DIRECTION: ")
 
 getRoutes = "https://svc.metrotransit.org/NexTrip/Routes?format=json"
-getDirections = "https://svc.metrotransit.org/NexTrip/Directions/5?format=json"
-getStops = "https://svc.metrotransit.org/NexTrip/Stops/5/4?format=json"
-getDepartures = "https://svc.metrotransit.org/NexTrip/11167?format=json"
-getTimepointDepartures = "https://svc.metrotransit.org/NexTrip/5/4/7SOL?format=json"
+getRoutes_response = requests.get(getRoutes).json()
+route = int(getRoutes_response[0]["Route"])
 
-print(requests.get(getRoutes).json())
-print(requests.get(getDirections).json())
-print(requests.get(getStops).json())
-print(requests.get(getDepartures).json())
-print(requests.get(getTimepointDepartures).json())
+getDirections = f"https://svc.metrotransit.org/NexTrip/Directions/{route}?format=json"
+# takes Route number
+getDirections_response = requests.get(getDirections).json()
+direction = int(getDirections_response[1]["Value"])
+
+getStops = f"https://svc.metrotransit.org/NexTrip/Stops/{route}/{direction}?format=json"
+# take Route number and Direction
+getStops_response = requests.get(getStops).json()
+stop = getStops_response[1]["Value"]
+
+getTimepointDepartures = f"https://svc.metrotransit.org/NexTrip/{route}/{direction}/{stop}?format=json"
+# take Route number, direction, stop value
+getTimepointDepartures_response = requests.get(getTimepointDepartures).json()
+nextBus = getTimepointDepartures_response[0]["DepartureText"]
+# “METRO Blue Line” “Target Field Station Platform 1” “south”
 
